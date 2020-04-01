8b0d6dd833dcec6bd4d4393de7a403503fc6f96e
Gagoilkim <dkim558@wisc.edu>
2020-03-31 21:07:02 -0500

    activitymain3 working

--- app/src/main/java/com/example/youandi/Main2Activity.java
+++ app/src/main/java/com/example/youandi/Main2Activity.java
@@ -2,7 +2,10 @@ package com.example.youandi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void clickFunction(View view){
+        EditText et = (EditText) findViewById(R.id.editText);
+        goToAct3();
+
+    }
+
+    public void goToAct3(){
+        Intent intent = new Intent(this, Main3Activity.class);
+        startActivity(intent);
     }
 }
