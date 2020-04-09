d4582496817b1429f8cc5c6edfb680118744fe7d
Yeochan Youn <yyoun5@wisc.edu>
2020-04-09 14:40:41 -0500

    move main2 layout to home layout

--- app/src/main/java/com/example/andorid/youandi/Main2Activity.java
+++ app/src/main/java/com/example/andorid/youandi/Main2Activity.java
@@ -3,6 +3,7 @@ package com.example.andorid.youandi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void clickFunction(View view){
     }
 }
