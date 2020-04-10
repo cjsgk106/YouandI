d43e8a4b2a802283810c2734799e7d2d40823555
Gagoilkim <dkim558@wisc.edu>
2020-04-09 23:09:25 -0500

    navigationbar layout

---
+++ app/src/main/java/com/example/andorid/youandi/model/ImageAdapter.java
@@ -0,0 +1,35 @@
+package com.example.andorid.youandi.model;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class ImageAdapter extends BaseAdapter {
+
+    private Context mContext;
+
+    public int[] imageArray={
+
+    };
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
