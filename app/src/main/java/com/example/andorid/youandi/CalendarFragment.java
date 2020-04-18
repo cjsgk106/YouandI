1e61c753e2c413781e7391cfa01de8ce556bcdbb
Gagoilkim <dkim558@wisc.edu>
2020-04-18 12:45:36 -0500

    Calendar layout

--- app/src/main/java/com/example/andorid/youandi/CalendarFragment.java
+++ app/src/main/java/com/example/andorid/youandi/CalendarFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +14,16 @@ public class CalendarFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        CalendarView calendarView = view.findViewById(R.id.Calendar);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+
+            }
+        });
+
+    return view;
     }
 }
