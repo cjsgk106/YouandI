f70ac2a2aa25f0c5dfadc4f9811ebde2911423d3
Gagoilkim <dkim558@wisc.edu>
2020-04-18 09:59:24 -0500

    Profile works

--- app/src/main/java/com/example/andorid/youandi/HomeFragment.java
+++ app/src/main/java/com/example/andorid/youandi/HomeFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,7 +17,17 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        ImageButton button = (ImageButton) view.findViewById(R.id.imageButton6);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 
 
