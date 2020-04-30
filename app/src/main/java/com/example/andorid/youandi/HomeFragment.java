edcdd6b50b07107e4fde7d3b4a106021ad4cb06d
GERRARD <cjsgk106@gmail.com>
2020-04-30 18:20:35 -0500

    Theme color changed. Toolbar added in homefragment.

--- app/src/main/java/com/example/andorid/youandi/HomeFragment.java
+++ app/src/main/java/com/example/andorid/youandi/HomeFragment.java
@@ -5,17 +5,24 @@ import android.content.Context;
 import android.content.Intent;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 
@@ -25,6 +32,8 @@ public class HomeFragment extends Fragment {
     ImageButton btn;
     TextView textView;
     String shared = "Shared";
+
+    private FirebaseAuth firebaseAuth;
 
     @Nullable
     @Override
@@ -44,9 +53,33 @@ public class HomeFragment extends Fragment {
        textView = (TextView) view.findViewById(R.id.textView);
         textView.setText(sharedPreferences.getString("DATE", ""));
 
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.fragment_home_toolbar);
+        toolbar.setBackgroundColor(Color.parseColor("#f7dce2"));
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        setHasOptionsMenu(true);
+
 
         return view;
     }
 
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.setting_actionbar, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_logout:
+                firebaseAuth = firebaseAuth.getInstance();
+                firebaseAuth.signOut();
+                startActivity(new Intent(getActivity(), LoginActivity.class));
+                return true;
+            case R.id.action_setting:
+                return true;
+        }
+        return false;
+    }
 }
 
--- app/src/main/java/com/example/andorid/youandi/NavigationActivity.java
+++ app/src/main/java/com/example/andorid/youandi/NavigationActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 
