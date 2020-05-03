36e1f78f48adf0f4d9c34ad15426ebf0cb78d55b
Gagoilkim <dkim558@wisc.edu>
2020-05-03 09:57:41 -0500

    Location Firebase

--- app/src/main/java/com/example/andorid/youandi/location/LocationViewActivity.java
+++ app/src/main/java/com/example/andorid/youandi/location/LocationViewActivity.java
@@ -26,8 +26,11 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -61,11 +64,10 @@ public class LocationViewActivity extends FragmentActivity implements OnMapReady
         addButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                String lata = Double.toString(lat);
-                String lona = Double.toString(lon);
-                if(lona.length() !=0 && lata.length() != 0){
-                    firebaseDatabase.child("users").child(firebaseAuth.getCurrentUser().getUid()).child("Location").child("longitude").setValue(lona);
-                    firebaseDatabase.child("users").child(firebaseAuth.getCurrentUser().getUid()).child("Location").child("latitude").setValue(lata);
+                String latlon = Double.toString(lat)+ " " + Double.toString(lon);
+                if(latlon.equals(" ")!= true ){
+                    firebaseDatabase.child("users").child(firebaseAuth.getCurrentUser().getUid()).child("Location").push().child("lat&lon").setValue(latlon);
+//                    firebaseDatabase.child("users").child(firebaseAuth.getCurrentUser().getUid()).child("Location").push().child("latitude").setValue(lata);
                     LatLng tmp = new LatLng(lat, lon);
                     arrayList.add(tmp);
 //                    mMap.addMarker(new MarkerOptions().position(tmp).title("visited"));
@@ -91,6 +93,24 @@ public class LocationViewActivity extends FragmentActivity implements OnMapReady
             }
         });
         Button showButton = (Button) findViewById(R.id.showButton);
+        DatabaseReference reference = firebaseDatabase.child("users").child(firebaseAuth.getCurrentUser().getUid()).child("Location").child("longitude");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                arrayList.clear();
+                for(DataSnapshot snapshot: dataSnapshot.getChildren()){
+                      String tmp=  snapshot.getValue().toString();
+                      String [] tmp2 = tmp.split(" ");
+                      LatLng latLng = new LatLng(Double.valueOf(tmp2[0]), Double.valueOf(tmp2[1]));
+                      arrayList.add(latLng);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
         showButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
